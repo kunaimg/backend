275390f8b0e11e77b0a48b82d27466d74a9e8e2d
kunaimg <kunalimg0@gmail.com>
2023-09-22 05:06:41 -0700

    sda

--- chat.js
+++ chat.js
@@ -1,11 +1,11 @@
-const http = require("http");
-const { Server } = require("socket.io");
+// const http = require("http");
+const socket = require("socket.io");
 const express = require("express");
 
 const path = require("path");
 const multer = require("multer");
 const app = express();
-const server = http.createServer();
+
 const cors = require("cors");
 app.use(express.json());
 
@@ -24,13 +24,6 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
-  },
-});
-
 app.post("/upload", upload.single("image"), (req, res) => {
   if (!req.file) {
     return res.status(400).send("No file uploaded.");
@@ -39,6 +32,12 @@ app.post("/upload", upload.single("image"), (req, res) => {
   const filePath = req.file.path;
 
   return res.json({ image: req.file.originalname });
+});
+const server = app.listen(3000, () => {});
+const io = socket(server, {
+  cors: {
+    origin: "*",
+  },
 });
 
 const user = {};
@@ -66,6 +65,3 @@ io.on("connection", (socket) => {
     });
   });
 });
-
-server.listen(2000, () => {});
-app.listen(3000, () => {});
