5ca3a9e67ea3dae4820fe1cd603ccf29f773a1e6
kunaimg <kunalimg0@gmail.com>
2023-09-24 10:36:43 -0700

    asdfasdfadsf

--- chatserver.js
+++ chatserver.js
@@ -1,18 +1,14 @@
-// const http = require("http");
-const socket = require("socket.io");
 const express = require("express");
-
-const path = require("path");
-const multer = require("multer");
+const socket = require("socket.io");
 const app = express();
-
 const cors = require("cors");
-app.use(express.json());
 
 app.use(cors());
+app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
-
+const path = require("path");
+const multer = require("multer");
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "uploads/");
@@ -23,10 +19,9 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
-const user = {};
+
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.post("/upload", upload.single("image"), (req, res) => {
-  user["image"] = req.file.originalname;
   if (!req.file) {
     return res.status(400).send("No file uploaded.");
   }
@@ -41,32 +36,17 @@ const io = socket(server, {
     origin: "*",
   },
 });
-
+let dataa;
 io.on("connection", (socket) => {
-  socket.emit("allvalues", {
-    name: user[socket.id],
-    image: user["image"],
+  socket.on("newuserjoin", (data) => {
+    dataa = data;
+    socket.broadcast.emit("usermessagedekho", data);
   });
-  socket.on("newuser-join", (obje) => {
-    user[socket.id] = obje.name;
-
-    socket.broadcast.emit("sabhidekhonewuser", {
-      name: user[socket.id],
-      image: user["image"],
-    });
-    socket.on("send", (message) => {
-      socket.broadcast.emit("usermessagedekho", {
-        message: message,
-        name: user[socket.id],
-        image: user["image"],
-      });
-    });
+  socket.on("message", (datas) => {
+    console.log(datas);
+    socket.broadcast.emit("messagee", datas);
   });
-
   socket.on("disconnect", () => {
-    socket.broadcast.emit("sabhidekhonewuserr", {
-      name: user[socket.id],
-      image: user["image"],
-    });
+    socket.broadcast.emit("disusermessagedekho", dataa);
   });
 });
